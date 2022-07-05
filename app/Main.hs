7e3be1a1c27aba2772a16d5969ef76370e5a883c
daniel <danieln@mercury.com>
2022-07-05 16:23:08 -0400

    basic game menu

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,18 @@ module Main where
 
 import Lib
 
+gameMenu :: ()
+gameMenu = 
+    putStrLn "Want to play?"
+    putStrLn "1. Yes"
+    putStrLn "2. No (exit)"
+    choice <- getLine :: Integer
+    if choice == 1
+        then startRound
+        else putStrLn "Thanks for playing!"
+            
 --
 -- MAIN
 --
 main = do
-  putStrLn "MAIN"
-  -- TODO main game loop
+    gameMenu
