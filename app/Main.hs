d68f02347c16cd667de40ba43eff4a9d3315690c
daniel <danieln@mercury.com>
2022-08-31 14:32:25 -0400

    added entity

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,27 @@
-module Main where
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
+module Main where
 
 import Application
 import Yesod
 import Control.Monad.Logger (runStderrLoggingT)
 import Database.Persist.Postgresql 
+import Data.Text
+
+newtype Name = Name { name :: Text } deriving newtype (Show, PersistField, PersistFieldSql)
+newtype Email = Email { email :: Text } deriving newtype (Show, PersistField, PersistFieldSql)
+newtype Password = Password { password :: Text } deriving newtype (Show, PersistField, PersistFieldSql)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+User
+    name Name
+    email Email
+    UniqueEmail email
+    hashedPassword Password
+    deriving Show
+|]
 
 connStr :: ConnectionString
 connStr = "host=localhost dbname=test user=test password=test port=5432"
